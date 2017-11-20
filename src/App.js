8a4a48d555619d7ada0d1ab2f9c609a2c89e4a7a
Eduardo Moroni <eduardomoroni@gmail.com>
2017-11-20 16:52:42 -0200

    refactor: change js files under src folder

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react'
 import {
   Platform,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
 import Config from 'react-native-config'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+    'Shake or press menu button for dev menu'
+})
 
-export default class App extends Component<{}> {
-  render() {
+export default class App extends React.Component<{}> {
+  render () {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
@@ -37,7 +31,7 @@ export default class App extends Component<{}> {
           {`this is a secret: ${Config.TEST}`}
         </Text>
       </View>
-    );
+    )
   }
 }
 
@@ -46,16 +40,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
