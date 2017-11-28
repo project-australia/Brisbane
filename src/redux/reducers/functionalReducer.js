0a1482d4e69eca98e38efa8e29e9db9187327687
Eduardo Moroni <eduardomoroni@gmail.com>
2017-11-28 20:47:54 -0200

    fix: returning initial state every time

--- src/redux/reducers/functionalReducer.js
+++ src/redux/reducers/functionalReducer.js
@@ -2,9 +2,9 @@ import Maybe from 'data.maybe'
 
 export function createReducer (reducerConfig) {
   const { actionHandlers, initialState } = reducerConfig
-  return (state, action) =>
+  return (state = initialState, action) =>
     Maybe
       .fromNullable(actionHandlers[action.type])
       .map(handler => handler(state, action))
-      .getOrElse(initialState)
+      .getOrElse(state)
 }
