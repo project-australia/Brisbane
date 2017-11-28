c221dd22e5c8922b8f0a47242c6759026977814f
Eduardo Moroni <eduardomoroni@gmail.com>
2017-11-28 10:02:57 -0200

    feat: add redux-logger

--- src/config/redux.js
+++ src/config/redux.js
@@ -1,9 +1,20 @@
-import {applyMiddleware, createStore as create} from 'redux'
-import {rootReducer} from '../redux/reducers'
 import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import {applyMiddleware, createStore as create} from 'redux'
 
-const initialState = {}
+import {rootReducer} from '../redux/reducers'
+
+const removeNavField = (state) => {
+  const {nav, ...rest} = state
+  return rest
+}
+
+const logger = createLogger({
+  duration: true,
+  collapsed: true,
+  stateTransformer: removeNavField
+})
 
 export const createStore = () => {
-  return create(rootReducer, initialState, applyMiddleware(thunk))
+  return create(rootReducer, applyMiddleware(logger, thunk))
 }
