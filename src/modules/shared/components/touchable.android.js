99c65c89441cf27b7e51c189ab71c8a7c27f736b
Bruno Talhate <btalhate@gmail.com>
2017-12-19 21:26:48 -0200

    Added to borderless property to android touchable

--- src/modules/shared/components/touchable.android.js
+++ src/modules/shared/components/touchable.android.js
@@ -2,8 +2,11 @@ import React from 'react'
 import {TouchableNativeFeedback, View} from 'react-native'
 
 export const Touchable = (props) => {
+  const background = (props.borderless)
+    ? TouchableNativeFeedback.SelectableBackgroundBorderless()
+    : TouchableNativeFeedback.SelectableBackground()
   return (
-    <TouchableNativeFeedback onPress={props.onPress} {...props}>
+    <TouchableNativeFeedback background={background} onPress={props.onPress} {...props}>
       <View style={props.style}>
         {props.children}
       </View>
