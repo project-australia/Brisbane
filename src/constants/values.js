c297fc99755f2bde09a9bcfcc6086760464212a7
Bruno Talhate <btalhate@gmail.com>
2017-12-19 21:23:28 -0200

    Added metrics and values

--- src/constants/values.js
+++ src/constants/values.js
@@ -1,9 +1,27 @@
 import {Platform, TouchableNativeFeedback} from 'react-native'
 
+import {Colors} from './colors'
+
 const BackgroundBorderlessRipple = (Platform.OS === 'android')
   ? TouchableNativeFeedback.SelectableBackgroundBorderless()
   : null
 
+const elevation1 = {
+  ...Platform.select({
+    android: {
+      elevation: 1
+    },
+    ios: {
+      shadowColor: Colors.gray900,
+      shadowOffset: {width: 0, height: 1},
+      shadowRadius: 0,
+      shadowOpacity: 0.12,
+      overflow: 'visible'
+    }
+  })
+}
+
 export const Values = {
-  BackgroundBorderlessRipple
+  BackgroundBorderlessRipple,
+  elevation1
 }
