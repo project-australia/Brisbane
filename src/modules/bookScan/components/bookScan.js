bac4aa8e0361aca026a58cf0e78f1222c7c2c49f
Eduardo Moroni <eduardomoroni@gmail.com>
2017-12-19 11:28:18 -0200

    refactor: dont take pictures at all

--- src/modules/bookScan/components/bookScan.js
+++ src/modules/bookScan/components/bookScan.js
@@ -5,20 +5,12 @@ import { Scanner } from '../../shared/components/camera'
 import { styles } from './styles/bookScan.style'
 
 export class BookScanner extends Component {
-  takePicture = () => {
-    const options = {}
-    alert('tirou foto')
-    this.camera.capture({metadata: options})
-      .then((data) => console.log(data))
-      .catch(err => console.error(err))
-  }
-
   render () {
     return (
       <View style={styles.container}>
         <Scanner style={styles.preview}>
-          <Text style={styles.capture} onPress={this.takePicture}>
-            [Scan]
+          <Text style={styles.capture}>
+            [INSIRA AQUI CUSTOMIZACOES]
           </Text>
         </Scanner>
       </View>
