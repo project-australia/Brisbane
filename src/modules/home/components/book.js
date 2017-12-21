c9205353520b268f30690f6ce121834311d0f711
Eduardo Moroni <eduardomoroni@gmail.com>
2017-12-21 12:13:14 -0200

    refactor(Book): make it stateless functional component

--- src/modules/home/components/book.js
+++ src/modules/home/components/book.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { book } from '../propTypes/book'
 import { Text, View, Image } from 'react-native'
 
@@ -7,24 +7,19 @@ import { FormButton, FormOutlineButton } from '../../shared/components/buttons'
 
 import { styles } from './styles/home.styles'
 
-export class Book extends Component {
-  static propTypes = { book: book.isRequired }
+export const Book = ({ book: { title, author, edition, imageUri } }) => (
+  <Card style={styles.bookCard}>
+    <Image style={styles.bookImage} source={{ uri: imageUri }} />
+    <View style={styles.bookInfoWrap}>
+      <Text style={styles.primaryText}>{title}</Text>
+      <Text style={styles.secondaryInput}>{author}</Text>
+      <Text style={styles.secondaryInput}>{edition}</Text>
+      <View style={styles.bookButtonsGroup}>
+        <FormButton style={styles.buyButton} title={'Buy'} />
+        <FormOutlineButton style={styles.rentButton} title={'Rent'} />
+      </View>
+    </View>
+  </Card>
+)
 
-  render () {
-    const { title, author, edition, imageUri } = this.props.book
-    return (
-      <Card style={styles.bookCard}>
-        <Image style={styles.bookImage} source={{ uri: imageUri }} />
-        <View style={styles.bookInfoWrap}>
-          <Text style={styles.primaryText}>{title}</Text>
-          <Text style={styles.secondaryInput}>{author}</Text>
-          <Text style={styles.secondaryInput}>{edition}</Text>
-          <View style={styles.bookButtonsGroup}>
-            <FormButton style={styles.buyButton} title={'Buy'} />
-            <FormOutlineButton style={styles.rentButton} title={'Rent'} />
-          </View>
-        </View>
-      </Card>
-    )
-  }
-}
+Book.propTypes = { book: book.isRequired }
