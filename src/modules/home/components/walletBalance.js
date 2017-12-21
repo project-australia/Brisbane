ef188e2abc7af21a0a6f17b83e9633fb0db5cc20
Eduardo Moroni <eduardomoroni@gmail.com>
2017-12-21 13:17:26 -0200

    refactor(WalletBalance): make it stateless functional component

--- src/modules/home/components/walletBalance.js
+++ src/modules/home/components/walletBalance.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Text, View } from 'react-native'
 import PropTypes from 'prop-types'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
@@ -9,38 +9,34 @@ import { CardFooterButton } from '../../shared/components/buttons'
 import { styles } from './styles/home.styles'
 import { Colors, Metrics } from '../../../constants'
 
-export class WalletBalance extends Component {
-  static propTypes = {
-    onWithDrawPressed: PropTypes.func.isRequired,
-    balance: PropTypes.number.isRequired
-  }
+export const WalletBalance = props => {
+  const titleButton = { text: 'View', onPress: props.onWalletViewPressed }
 
-  render () {
-    const titleButton = {
-      text: 'View',
-      onPress: this.props.onWalletViewPressed
-    }
-    return (
-      <View>
-        <MenuTitle title={'My Wallet'} button={titleButton} />
-        <View style={[styles.card, styles.square]}>
-          <View style={styles.menuColumn}>
-            <View style={styles.featuredIconWrap}>
-              <Icon
-                name={'wallet'}
-                color={Colors.gray200}
-                size={Metrics.icons.large}
-              />
-            </View>
-            <Text style={styles.secondaryInput}>Balance</Text>
-            <Text style={styles.primaryText}>${this.props.balance}</Text>
+  return (
+    <View>
+      <MenuTitle title={'My Wallet'} button={titleButton} />
+      <View style={[styles.card, styles.square]}>
+        <View style={styles.menuColumn}>
+          <View style={styles.featuredIconWrap}>
+            <Icon
+              name={'wallet'}
+              color={Colors.gray200}
+              size={Metrics.icons.large}
+            />
           </View>
-          <CardFooterButton
-            onPress={this.props.onWithDrawPressed}
-            title={'Withdraw'}
-          />
+          <Text style={styles.secondaryInput}>Balance</Text>
+          <Text style={styles.primaryText}>${props.balance}</Text>
         </View>
+        <CardFooterButton
+          onPress={props.onWithDrawPressed}
+          title={'Withdraw'}
+        />
       </View>
-    )
-  }
+    </View>
+  )
+}
+
+WalletBalance.propTypes = {
+  onWithDrawPressed: PropTypes.func.isRequired,
+  balance: PropTypes.number.isRequired
 }
