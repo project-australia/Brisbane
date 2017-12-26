cf1b8926c6d7c99e7fd1e9d36a97c9bcef619106
Eduardo Moroni <eduardomoroni@gmail.com>
2017-12-26 12:07:34 -0200

    build: fix ebert issue

--- src/domain/ShoppingBagItem.js
+++ src/domain/ShoppingBagItem.js
@@ -1,10 +1,10 @@
 export const SHOPPING_BAG_TYPES = { BUY: 'BUY', RENT: 'RENT' }
 
 export class ShoppingBagItem {
-  constructor (item, type) {
+  constructor (item, type, quantity = 1) {
     this.item = item
     this.type = SHOPPING_BAG_TYPES[type]
-    this.quantity = 1
+    this.quantity = quantity
 
     if (!this.type) {
       throw new Error('Invalid Shopping bag item type')
