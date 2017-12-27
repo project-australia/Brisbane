d0c03144c6c910a98ff840cf867f73b7a3c08b81
Hebert Porto <hebertporto@gmail.com>
2017-12-27 13:52:24 -0200

    Fix: Varias chamdas para bookConfirmation

--- src/modules/bookScan/components/scanner.js
+++ src/modules/bookScan/components/scanner.js
@@ -11,7 +11,18 @@ export class Scanner extends Component {
     showBook: PropTypes.func.isRequired
   }
 
-  getSellingPrice = (isbn) => {
+  state = {
+    isReading: false
+  }
+
+  onRead = (isbn) => {
+    if (!this.state.isReading) {
+      this.getSellingPrice(isbn)
+    }
+  }
+
+  getSellingPrice = async (isbn) => {
+    await this.setState({ isReading: true })
     this.props.getQuote(isbn)
     this.props.showBook()
   }
@@ -22,7 +33,7 @@ export class Scanner extends Component {
         <Camera
           style={styles.scanner}
           aspect={Camera.constants.Aspect.fill}
-          onBarCodeRead={e => this.getSellingPrice(e.data)}
+          onBarCodeRead={e => this.onRead(e.data)}
         />
       </View>
     )
