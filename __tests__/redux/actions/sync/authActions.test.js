907f628cb01e9a41699e8f145d9048efb7a38061
Eduardo Moroni <eduardomoroni@gmail.com>
2017-12-29 08:24:51 -0200

    feat: add action to update user s profile

--- __tests__/redux/actions/sync/authActions.test.js
+++ __tests__/redux/actions/sync/authActions.test.js
@@ -3,6 +3,7 @@ import {
   showAlert,
   signInSuccess,
   signUpSuccess,
+  updateUserProfile,
   successRetrievedPassword
 } from '../../../../src/redux/actions/sync/authenticationActions'
 import {
@@ -10,31 +11,58 @@ import {
   FORGOT_PASSWORD_SUCCESS,
   SHOW_ALERT,
   SIGN_IN_SUCCESS,
-  SIGN_UP_SUCCESS
+  SIGN_UP_SUCCESS,
+  UPDATE_USER_INFO
 } from '../../../../src/redux/types/authenticationTypes'
 
+const user = {
+  id: '2Cbqh6mjOGUkb9Vsu3M42oPJW5V2',
+  referId: 'HEBERT_BOLADO',
+  referredBy: 'DUDUZINHO',
+  name: 'talhate',
+  email: 't@yahoo.com',
+  birthDate: '2017-12-27T17:00:04.376Z',
+  telephone: '1234567890',
+  school: 'school of life',
+  club: 'NONE',
+  role: 'USER',
+  address: {
+    city: 'viana',
+    street: 'fighter',
+    number: '666',
+    zipCode: 'zip',
+    state: 'es'
+  }
+}
+
 describe('Auth sync actions', () => {
-  it('Should send a sucessfull forgot password action', async () => {
+  it('should create an action to update user information', () => {
+    const expectedAction = { type: UPDATE_USER_INFO, user }
+    const action = updateUserProfile(user)
+    expect(action).toEqual(expectedAction)
+  })
+
+  it('Should send a sucessfull forgot password action', () => {
     const message = 'successfull message'
     const expectedAction = { type: FORGOT_PASSWORD_SUCCESS, message }
     const action = successRetrievedPassword(message)
     expect(action).toEqual(expectedAction)
   })
 
-  it('Should create show alert action', async () => {
+  it('Should create show alert action', () => {
     const message = 'successfull message'
     const expectedAction = { type: SHOW_ALERT, message }
     const action = showAlert(message)
     expect(action).toEqual(expectedAction)
   })
 
-  it('Should send a sucessfull signup action', async () => {
+  it('Should send a sucessfull signup action', () => {
     const expectedAction = { type: SIGN_UP_SUCCESS }
     const action = signUpSuccess()
     expect(action).toEqual(expectedAction)
   })
 
-  it('Should send a sucessfull signin action', async () => {
+  it('Should send a sucessfull signin action', () => {
     const user = {
       uid: 'UID',
       emailVerified: false,
