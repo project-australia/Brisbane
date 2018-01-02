8b2f61fac3289ae67c21f25b7040da03bde17931
Eduardo Moroni <eduardomoroni@gmail.com>
2018-01-02 12:22:24 -0200

    fix: ci build

--- src/services/backend/userService.js
+++ src/services/backend/userService.js
@@ -1,11 +1,10 @@
-import { userProfile } from '../../../__tests__/fixtures/userProfile'
 
 // TODO: Esse metodo tem que chamar o back end e retornar a representacao de user
 export const signUpUser = async (signUpForm) => {
-  return { ...userProfile }
+  return {}
 }
 
 // TODO: Esse metodo tem que chamar o back end e retornar a representacao de user
 export const getUserProfile = async (userId) => {
-  return { ...userProfile, id: userId }
+  return { id: userId }
 }
