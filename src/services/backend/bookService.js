30a4bbe2491877c50f0d899168a0d433636b5a47
Eduardo Moroni <eduardomoroni@gmail.com>
2018-01-27 12:41:58 -0200

    refactor: use then to make funcion one line

--- src/services/backend/bookService.js
+++ src/services/backend/bookService.js
@@ -1,11 +1,4 @@
 import Axios from 'axios'
 
-export const recentlyAddedBooks = async () => {
-  const response = await Axios.get('/books/recentlyAddedBooks')
-  return response.data
-}
-
-export const featuredBooks = async () => {
-  const response = await Axios.get('/books/featuredBooks')
-  return response.data
-}
+export const recentlyAddedBooks = async () => Axios.get('books/recentlyAdded').then(res => res.data)
+export const featuredBooks = async () => Axios.get('books/featured').then(res => res.data)
