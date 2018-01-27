20f02fc2b1a0443e5a30320c9032c2ab5f5da594
Hebert Porto <hebertporto@gmail.com>
2018-01-27 09:22:59 -0200

    redirect para home

--- src/modules/home/containers/walletBalanceContainer.js
+++ src/modules/home/containers/walletBalanceContainer.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Alert } from 'react-native'
 import { connect } from 'react-redux'
 import { WalletBalance } from '../components/walletBalance'
 
+const confirmation = () => (
+  Alert.alert(
+    'Withdraw Solicitation',
+    'Do you want request your credit?',
+    [
+      {text: "Yes, I'm sure", onPress: () => console.log('OK Pressed')},
+      {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'}
+    ],
+    { cancelable: false }
+  )
+)
 const WalletContainer = () => (
   <WalletBalance
     balance={15.5}
-    onWithDrawPressed={() => alert('💸 money gone!')}
+    onWithDrawPressed={confirmation}
     onWalletViewPressed={() => alert('🛶 navigate to wallet')}
   />
 )
