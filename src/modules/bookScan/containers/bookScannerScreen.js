9a33ea6f167fd0dbbdaa7f3cdac7e31ba6196490
Hebert Porto <hebertporto@gmail.com>
2018-02-22 18:08:21 -0300

    fix undefined on scan book

--- src/modules/bookScan/containers/bookScannerScreen.js
+++ src/modules/bookScan/containers/bookScannerScreen.js
@@ -8,7 +8,7 @@ class BookScanner extends Component {
     title: 'Book Scan'
   }
   navigateToBookSelling = isbn =>
-    this.props.navigation.navigate('BookDetails', { isbn })
+    this.props.navigation.navigate('BookDetails', { isbn, screenType: 'SELL' })
 
   render() {
     return <Scanner showBook={this.navigateToBookSelling} />
