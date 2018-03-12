aeadd9c26927ccecdae98789d44d7dd360c78b75
Bruno Talhate <btalhate@gmail.com>
2018-03-11 22:48:06 -0300

    dropdown button added

--- src/modules/shared/components/rowValue.js
+++ src/modules/shared/components/rowValue.js
@@ -12,12 +12,20 @@ const renderSubtitle = subtitle => {
   return null
 }
 
+const renderValue = value => {
+  if (typeof value === 'string') {
+    return <Text style={styles.rightValue}>{value}</Text>
+  }
+  if (typeof value === 'object') return value
+  return null
+}
+
 export const RowValue = ({ title, subtitle, value, onPress, onPressTitle }) => (
   <TouchableIfOnPress onPress={onPress} style={styles.wrap}>
     <TouchableIfOnPress onPress={onPressTitle} style={styles.sideContentWrap}>
       <Text style={styles.contentTitle}>{title}</Text>
       {renderSubtitle(subtitle)}
     </TouchableIfOnPress>
-    <Text style={styles.rightValue}>{value}</Text>
+    {renderValue(value)}
   </TouchableIfOnPress>
 )
