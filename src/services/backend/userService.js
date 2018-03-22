b622d42a3fb6446aaff3dbe06215b08046afbe27
Eduardo Moroni <eduardomoroni@gmail.com>
2018-03-22 12:19:36 -0300

    refactor: user service to use a cleaner syntax

--- src/services/backend/userService.js
+++ src/services/backend/userService.js
@@ -6,35 +6,11 @@ const SIGNUP_ERROR_MESSAGE =
   'Something wrong has happened on your sign up, please try it again in a few minutes'
 const SIGNUP_BAD_REQUEST_MESSAGE = 'Please Check your form inputs'
 
-export const signUpUser = async signUpForm => {
-  try {
-    const response = await Axios.post('/users', signUpForm)
-    return mapToUserProfile(response)
-  } catch (err) {
-    handleError(err)
-  }
-}
-
-export const getUserProfile = async userId => {
-  try {
-    const response = await Axios.get(`/users/${userId}/profile`)
-    return mapToUserProfile(response)
-  } catch (err) {
-    handleError(err)
-  }
-}
-
-export const putUserProfile = async (id, profile) =>
-  Axios.put(`/users/${id}/profile`, profile)
-    .then(mapToUserProfile)
-    .catch(handleError)
-
-export const wakeUpBackEnd = () => Axios.get('/health').catch(console.info)
-
-export const requestWithdraw = (userId, walletPaypalAccount) =>
-  Axios.put(`/users/${userId}/profile`, walletPaypalAccount).catch(err =>
-    handleError(err)
-  )
+export const wakeUpBackEnd = () => Axios.get('/health').catch(console.warn)
+export const requestWithdraw = (userId, wallet) => Axios.put(`/users/${userId}/profile`, wallet).catch(handleError)
+export const signUpUser = async signUpForm => Axios.post('/users', signUpForm).then(mapToUserProfile).catch(handleError)
+export const getUserProfile = async userId => Axios.get(`/users/${userId}/profile`).then(mapToUserProfile).catch(handleError)
+export const putUserProfile = async (id, profile) => Axios.put(`/users/${id}/profile`, profile).then(mapToUserProfile).catch(handleError)
 
 const handleError = err => {
   if (!err.response) {
