7a70b250b2cdd5e8a70ea0d70d04c524695c3563
Eduardo Moroni <eduardomoroni@gmail.com>
2018-03-24 07:46:32 -0300

    gambeta: force BookScanner to NOT call navigate multiple times

--- src/modules/bookScan/containers/bookScannerScreen.js
+++ src/modules/bookScan/containers/bookScannerScreen.js
@@ -3,20 +3,32 @@ import { connect } from 'react-redux'
 
 import { Scanner } from '../components/scanner'
 
+let gambiarra = false
+let intervalID = 0
+
 class BookScanner extends Component {
   static navigationOptions = {
     title: 'Book Scan'
   }
 
   state = {
-    isReading: false,
-    isbn: null
+    isReading: false
+  }
+
+  componentDidMount() {
+    // FIXME: Por algum motivo, o Scanner chama o navigate varias vezes
+    intervalID = setInterval(() => { gambiarra = false }, 1500)
+  }
+
+  componentWillUnmount() {
+    clearInterval(intervalID)
   }
 
   onRead = isbn => {
-    if (!this.state.isReading) {
+    if (!this.state.isReading && !gambiarra) {
+      gambiarra = true
       this.setState({ isReading: true })
-      this.navigateToBookDetails(isbn)
+      gambiarra && this.navigateToBookDetails(isbn)
       this.setState({ isReading: false })
     }
   }
