74c09f91cbb30ff46f3268038f81c1010a096dd3
Bruno Talhate <btalhate@gmail.com>
2018-04-09 23:10:15 -0300

    Scanner screen top and bottom bar interfaces

--- src/modules/bookScan/components/scanner.js
+++ src/modules/bookScan/components/scanner.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import Camera from 'react-native-camera'
 import PropTypes from 'prop-types'
+
+import { AppStatusBar } from '../../shared/components/appStatusBar'
+import { Text, View } from 'react-native'
 import { LoadingOverlay } from '../../shared/components/loadingOverlay'
+import { Touchable } from '../../shared/components/touchable'
 
 import { styles } from './styles/bookScanner.style'
 
 export class Scanner extends Component {
   static propTypes = {
+    onCancel: PropTypes.func.isRequired,
     onRead: PropTypes.func.isRequired,
     isLoading: PropTypes.bool.isRequired
   }
@@ -14,11 +19,21 @@ export class Scanner extends Component {
   render() {
     return (
       <LoadingOverlay isLoading={this.props.isLoading} style={styles.container}>
+        <AppStatusBar />
         <Camera
           style={styles.scanner}
           aspect={Camera.constants.Aspect.fill}
           onBarCodeRead={e => this.props.onRead(e.data)}
         />
+        <View style={styles.darkTopOverlay}>
+          <Text style={styles.overlayTitle}>Scan the book ISBN</Text>
+          <Text style={styles.overlayDescription}>Place the full barcode on the center of the screen</Text>
+        </View>
+        <View style={styles.darkBottomOverlay}>
+          <Touchable onPress={this.props.onCancel} style={styles.cancelButtonTouchArea}>
+            <Text style={styles.overlayTitle}>Cancel</Text>
+          </Touchable>
+        </View>
       </LoadingOverlay>
     )
   }
